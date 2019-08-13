5e65c4ea5fdd3a29e6444a587b1e9bd33628fd8f
Christophe Marchand <christophe@marchand.top>
2019-08-13 18:01:23 +0200

    Fixed javadoc

--- src/main/java/top/marchand/java/env/viewer/EnvViewer.java
+++ src/main/java/top/marchand/java/env/viewer/EnvViewer.java
@@ -1,23 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 package top.marchand.java.env.viewer;
 
 /**
- *
+ * This class print to standard output
+ * all java System properties, and all
+ * environmenet entries.
+ * 
+ * Java System properties are prefixed by {@code ssp.}
+ * Environmenet entries are prefixed by {@code env.}
+ * 
+ * Hence, it's easy to filter / sort output
  * @author cmarchand
  */
 public class EnvViewer {
     
     public static void main(String[] args) {
-        //System.out.println("*** System properties ***");
         System.getProperties().keySet().forEach((key) -> {
             System.out.println("ssp."+key.toString()+"="+System.getProperty(key.toString()));
         });
-        //System.out.println();
-        //System.out.println("*** Environment ***");
         System.getenv().keySet().forEach((key) -> {
             System.out.println("env."+key+"="+System.getenv(key));
         });
